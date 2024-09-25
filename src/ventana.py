e64f0eedb91553190423a62b25f9160323ce3fdc
Esus <133167283+UriiTogni@users.noreply.github.com>
2024-09-24 21:58:06 -0300

    Add files via upload

---
+++ src/ventana.py
@@ -0,0 +1,12 @@
+import pygame
+
+# Configuración de la pantalla y fondo
+FPS = 60
+WIDTH = 800
+HEIGTH = 600
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+screen = pygame.display.set_mode((WIDTH, HEIGTH))
+pygame.display.set_caption("Michi espacial")
+background_Image = pygame.image.load("./images/Nebula.png").convert()
+background_Rect = background_Image.get_rect()
